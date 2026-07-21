82038a212e0054f16583b9bd208f2d947e75a781
narielyenesel <nariely.pineda@eneselgroup.com>
2026-07-21 15:28:08 -0400

    Add webpack configuration to ignore canvas module

--- laytime-calculator/laytime-calculator/next.config.js
+++ laytime-calculator/laytime-calculator/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { webpack }) => {
+    config.resolve.fallback = { ...config.resolve.fallback, canvas: false };
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^canvas$/ }));
+    return config;
+  },
+};
 
 module.exports = nextConfig;
